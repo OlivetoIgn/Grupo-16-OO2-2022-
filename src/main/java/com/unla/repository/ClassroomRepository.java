14a3bcc7249b018586b1fcb7e67f057bde41fc45
Santiago Martinez <smartinezvila29@gmail.com>
2022-06-22 19:07:25 -0300

    Revert "Merge branch 'main' into develop"
    This reverts commit efb80d560337eeb64b38995a2b904ba583c9ee24, reversing
    changes made to ab77bf4f2d0da054b3e3b58f0e766eb8d7275a2b.

--- src/main/java/com/unla/repository/ClassroomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.unla.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.unla.entity.Classroom;
-
-public interface ClassroomRepository extends CrudRepository<Classroom, Long>{
-	
-}
